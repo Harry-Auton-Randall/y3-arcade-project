a17147909a03c208e1acb651f002dd297b15057a
Harry-Auton-Randall <hautonrandall@gmail.com>
2026-02-02 18:47:07 +0000

    New reticle that follows mouse movement along y=0

--- Assets/scripts/boatControlPlayer.cs
+++ Assets/scripts/boatControlPlayer.cs
@@ -3,17 +3,32 @@ using UnityEngine.InputSystem;
 
 public class boatControlPlayer : MonoBehaviour
 {
-    Vector2 mousePosition;
+    //Camera stuff
+    Vector2 mousePosition, mousePositionAdj;
     Vector2 screenSize;
-    float movement;
-    float rotation;
-
-    boatMove bm;
-    Transform camBase;
-    Transform camMove;
-
     Vector3 camPosition;
     Vector3 camBaseRotation;
+
+    Transform camBase;
+    Transform camMove;
+    Camera persCam;
+    Camera orthCam;
+    public bool cameraPers = true;
+    public bool cameraTilt = true;
+    public int cameraRotY = 0;
+    float cameraRotYGrad;
+
+    //reticle stuff
+    MeshCollider zeroPlane;
+    Transform reticle;
+    RaycastHit hit;
+    LayerMask oceanLayer;
+    Ray ray;
+
+    //Boat movement stuff
+    float movement;
+    float rotation;
+    boatMove bm;
 
     void Awake()
     {
@@ -22,6 +37,16 @@ public class boatControlPlayer : MonoBehaviour
         screenSize.y = Screen.height;
         camBase = transform.Find("CameraBase");
         camMove = transform.Find("CameraBase/CameraMove");
+        persCam = transform.Find("CameraBase/CameraMove/CameraRot/PersCamera").GetComponent<Camera>();
+
+        
+        camPosition = camMove.localPosition;
+
+        zeroPlane = GameObject.Find("ocean").GetComponent<MeshCollider>();
+        reticle = transform.Find("reticle");
+
+        oceanLayer |= (1 << 6);
+
     }
     void Start()
     {
@@ -41,21 +66,32 @@ public class boatControlPlayer : MonoBehaviour
 
     void Update()
     {
+        //rotates CamBase to be pointing up
         camBaseRotation = transform.localEulerAngles;
-        camBaseRotation.y = camBaseRotation.y * -1;
+        camBaseRotation.y = (camBaseRotation.y * -1) + cameraRotY;
         camBase.localEulerAngles = camBaseRotation;
 
+        //gets mousePosition and mousePositionAdj values
         mousePosition = Mouse.current.position.ReadValue();
-        mousePosition.x = Mathf.Clamp(((mousePosition.x * 2) / screenSize.x) - 1, -1, 1);
-        mousePosition.y = Mathf.Clamp(((mousePosition.y * 2) / screenSize.y) - 1, -1, 1);
+        mousePositionAdj.x = Mathf.Clamp(((mousePosition.x * 2) / screenSize.x) - 1, -1, 1);
+        mousePositionAdj.y = Mathf.Clamp(((mousePosition.y * 2) / screenSize.y) - 1, -1, 1);
 
-        camPosition = camMove.localPosition;
-        camPosition.x = mousePosition.x * 13.5f * (screenSize.x / screenSize.y);
-        camPosition.z = (mousePosition.y * 13.5f) - 3.25f;
+        //moves the camera with mousePosition
+        //camPosition = camMove.localPosition;
+        camPosition.x = mousePositionAdj.x * 13.5f * (screenSize.x / screenSize.y);
+        camPosition.z = (mousePositionAdj.y * 13.5f);
+        if (cameraPers && cameraTilt)
+        {
+            camPosition.z = camPosition.z - 3.25f;
+        }
         //13.5 = extra view distance in vertical direction. total view area = 2*(22.5+13.5) = 72 vertical and (72 * 16/9 =) 128m horizontal
         camMove.localPosition = camPosition;
 
-
-
+        ray = persCam.ScreenPointToRay(mousePosition);
+        Debug.DrawRay(ray.origin, ray.direction * 1000, Color.yellow);
+        if (Physics.Raycast(ray, out hit, Mathf.Infinity, oceanLayer))
+        {
+            reticle.transform.position = hit.point;
+        }
     }
 }
